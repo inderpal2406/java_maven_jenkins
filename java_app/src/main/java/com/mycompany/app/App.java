04cb07928b871b165d5dae0175bc60ba1fae82b3
Inderpal Singh <inderpal2406@gmail.com>
2020-01-19 20:26:16 +0530

    Update App.java and AppTest.java

--- java_app/src/main/java/com/mycompany/app/App.java
+++ java_app/src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,12 @@
 package com.mycompany.app;
 
 /**
- * Hello world!
+ * Hello Inderpal!
  */
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello Inderpal!";
 
     public App() {}
 
